4b0cf211b7adfa6ca2f704b6a5d80d40aa8ec456
avahak <a.vahakangas.work@gmail.com>
2026-03-31 22:41:34 +0300

    changed from react-hook-mathjax to better-react-mathjax

--- src/buildInfo.ts
+++ src/buildInfo.ts
@@ -1,1 +1,1 @@
-export const BUILD_TIMESTAMP = '31/03/2026, 21.48';
+export const BUILD_TIMESTAMP = '31/03/2026, 22.40';
--- src/dzhanibekov/App.tsx
+++ src/dzhanibekov/App.tsx
@@ -1,35 +1,56 @@
 import React, { Suspense } from 'react';
 import { Box, Container, Typography } from '@mui/material';
-import Tex2SVG from "react-hook-mathjax";
+import { MathJaxContext, MathJax } from 'better-react-mathjax';
 import { Link as RouterLink } from 'react-router-dom';
 import { Link as MUILink } from '@mui/material';
 const ThreeScene = React.lazy(() => import('./ThreeScene'));
 
+const config = {
+    tex: {
+        inlineMath: [
+            ["$", "$"],
+            ["\\(", "\\)"]
+        ],
+        displayMath: [
+            ["$$", "$$"],
+            ["\\[", "\\]"]
+        ]
+    }
+};
+
 const App: React.FC = () => {
     return (
         <Container maxWidth="xl">
-            <Box display="flex" justifyContent="center" sx={{py: 2}}>
+            <Box display="flex" justifyContent="center" sx={{ py: 2 }}>
                 <Typography variant="h2">
                     Dzhanibekov effect
                 </Typography>
             </Box>
             <Box style={{ width: "100%", height: "600px" }}>
-            <Suspense fallback={<Box display="flex" justifyContent="center"><Typography>Loading..</Typography></Box>}>
-                <ThreeScene />
-            </Suspense>
+                <Suspense fallback={<Box display="flex" justifyContent="center"><Typography>Loading..</Typography></Box>}>
+                    <ThreeScene />
+                </Suspense>
             </Box>
-            <Box display="flex" justifyContent="center" sx={{py: 2}}>
+            <Box display="flex" justifyContent="center" sx={{ py: 2 }}>
                 <Typography>
-                    Compare with <MUILink href={"https://en.wikipedia.org/wiki/File:Dzhanibekov_effect.ogv"}>video</MUILink> 
+                    Compare with <MUILink href={"https://en.wikipedia.org/wiki/File:Dzhanibekov_effect.ogv"}>video</MUILink>
                     {" "}on <MUILink href={"https://en.wikipedia.org/wiki/Tennis_racket_theorem"}>tennis racket theorem</MUILink> Wikipedia page.
                 </Typography>
             </Box>
-            <Box display="flex" justifyContent="center" sx={{py: 2}}>
-                <Typography>
-                    Equation for the rotation <Tex2SVG display="inline" latex="q\in\mathbb{H}" /> corresponding to matrix <Tex2SVG display="inline" latex="R" />:
-                    <Tex2SVG latex="\omega=RI_0^{-1}R^tI_0\omega_0," />
-                    <Tex2SVG latex="q'(t)=\frac{1}{2}\omega*q(t)." />
-                </Typography>
+            <Box display="flex" justifyContent="center" sx={{ py: 2 }}>
+                <MathJaxContext version={3} config={config}>
+                    <Typography>
+                        <MathJax hideUntilTypeset="first">
+                            {`
+                            Equation for the rotation $q \\in \\mathbb{H}$ corresponding to matrix $R$:
+
+                            $$\\omega = R I_0^{-1} R^t I_0 \\omega_0,$$
+
+                            $$q'(t) = \\frac{1}{2} \\omega * q(t).$$
+                            `}
+                        </MathJax>
+                    </Typography>
+                </MathJaxContext>
             </Box>
             <MUILink component={RouterLink} to="/" variant="body1" color="primary">
                 Back
