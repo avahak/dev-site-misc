84f04c768c371c665d8c734b9a052e2c67993a13
avahak <a.vahakangas.work@gmail.com>
2026-06-14 14:30:12 +0300

    changes to wood solid texture

--- src/buildInfo.ts
+++ src/buildInfo.ts
@@ -1,1 +1,1 @@
-export const BUILD_TIMESTAMP = '14/06/2026, 05.38';
+export const BUILD_TIMESTAMP = '14/06/2026, 14.29';
--- src/solid/shaders/solid/sWood.glsl
+++ src/solid/shaders/solid/sWood.glsl
@@ -1,5 +1,7 @@
 // Based on https://dl.acm.org/doi/10.1145/3528223.3530081, see also
 // https://www.youtube.com/watch?v=mMvoTtipJac https://www.shadertoy.com/view/fsyyzt
+// and https://github.com/marialarsson/procedural_knots
+
 
 
 struct Knot {
@@ -7,8 +9,13 @@ struct Knot {
     float death;        // in [0,1]
     float strength;     // in [0,1] to prevent overlapping branches
     vec3 dir;           // (knot direction \beta, knot asymptotic verticality a, knot instant verticality b)
-    float f;            // smoothness coefficient
     // etc.
+};
+
+struct LookupInfo {
+    float rStem;
+    vec2 pith;
+    Knot knot;
 };
 
 
@@ -35,11 +42,11 @@ float sminPow(float a, float b, float k) {
 
 
 vec2 getPith(float z) {
-    return 0.0*vnoise33(vec3(0.0, 0.0, z)).xy;
+    return 0.02*vnoise33(5.0*vec3(0.0, 0.0, z)).xy;
 }
 
 
-Knot knotLookupPlaceholder(vec3 p) {
+LookupInfo lookupPlaceholder(vec3 p) {
     // Here we should use texture lookups, etc. 
     // Instead we use hardcoded values for testing.
 
@@ -48,18 +55,23 @@ Knot knotLookupPlaceholder(vec3 p) {
     knot.death = debug2;
     knot.strength = 1.0;
     knot.dir = vec3(-PI/2.0, debug3, debug4);
-    knot.f = -debug1;
-    return knot;
+
+    LookupInfo lookup;
+    lookup.knot = knot;
+    lookup.pith = getPith(p.z);
+    lookup.rStem = (2.0 + 0.2*snoise(0.5*vec3(normalize(p.xy-lookup.pith), p.z))) / 3.0;
+
+    return lookup;
 }
 
 
 vec4 wood(vec3 p) {
     p = vec3(p.x, -p.z, p.y);        // TODO find more systematic way to do this
 
-    Knot knot = knotLookupPlaceholder(p);
-    vec2 pith = getPith(p.z);
+    LookupInfo lookup = lookupPlaceholder(p);
+    Knot knot = lookup.knot;
 
-    vec2 v = p.xy - pith;
+    vec2 v = p.xy - lookup.pith;
     float phi = atan(v.y, v.x);
     float r = length(v);
     // Now p is (r,phi,p.z) in cylinder coordinates.
@@ -73,35 +85,42 @@ vec4 wood(vec3 p) {
 
     float dPith = r;
     vec3 cp = closestRayPoint(p, knotP, vec3(knotDirXY, knot.dir.y));
-    float dRay = length(p - cp);
-    float dKnot = dRay;
+    float dKnot = distance(p, cp);      // or just: distance(p, knotP);
     
-    // For the "butterfly" effect (i.e. nonuniform deformation around the axis of the knot)
-    float cosBetaKnot = dot(vec3(-knotDirXY.y, knotDirXY.x, 0.0), p-cp) / dRay;
-    float xx = cosBetaKnot * cosBetaKnot;
-    float f = mix(-0.4, 0.1, xx);   // TODO improve
-    float fk = mix(10.0, 5.0, xx);  // TODO improve
+    vec3 betaVec = p - knotP;
+    float betaKnot = atan(betaVec.z, dot(betaVec.xy, vec2(-knotDirXY.y, knotDirXY.x)));
+    // float cosBetaKnot = cos(betaKnot);
+    float rKnot = 0.2 - 0.0*sqrt(dPith) + 0.02*snoise(1.0*vec3(cos(betaKnot), sin(betaKnot), dPith));
 
-    float t0 = dPith;
-    float t1 = 5.0 * dKnot;
-    float t = sminPow(t0, t1, 2.0);
+    float t0 = dPith / lookup.rStem;
+    float t1 = dKnot / rKnot;
 
+    float tDelta = t0 - t1;
+    float k = 1.75*tDelta/(0.3+abs(tDelta)) + 3.25;
+
+    float t = sminPow(t0, t1, k);
     float delta = t - min(t0, t1);
 
-    float isAlive = 1.0;
     
     // Death
+    float isAlive = 1.0;
     if (t > knot.death) {
         isAlive = 0.0;
-        float s = abs(t0 - knot.death);
-        float kDelta = 2.0 + fk*s;
-        t1 += s;
-        delta = f * (sminPow(t0, t1, kDelta) - min(t0, t1));
+        float tDead = abs(t0 - knot.death);
+        t1 += tDead;
+
+        float kDead = k + 5.0*tDead;
+        float t = sminPow(t0, t1, kDead);
+        float tempDelta = t - min(t0, t1);
+
+        float s = 8.0*tDead - 1.0;
+        float f = 0.35 - 0.85*s/(0.3+abs(s));
+        delta = f*tempDelta;
     }
     t = min(t0, t1) + delta;
 
-    t = mix(t0, t, knot.strength);
 
-    // return vec4(vec3(mix(0.0, 1.0, cosBetaKnot*cosBetaKnot), mix(0.0, 1.0, 1.0-cosBetaKnot*cosBetaKnot), 0.0), 1.0);
+    // t = mix(t0, t, knot.strength);
+
     return vec4(vec2(sin(100.0/(0.1+debug5)*t)), isAlive, 1.0);
 }
